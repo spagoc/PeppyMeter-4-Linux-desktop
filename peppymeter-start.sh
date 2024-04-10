d68776d68ae91905966d06d780b8b89de7afda4e
spagoc <spagoc@users.noreply.github.com>
2024-04-10 07:55:27 +0200

    Update peppymeter-start.sh
    Create the named pipe

--- peppymeter-start.sh
+++ peppymeter-start.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+PIPE_NAME="/tmp/peppymeterfifo"
+
+# Check if the named pipe already exists
+if [[ ! -p $PIPE_NAME ]]; then
+    # Create the named pipe
+    mkfifo $PIPE_NAME
+    
 alias activate=". ../pyenvs/pygame/bin/activate"
 cd
 cd PeppyMeter-master/
